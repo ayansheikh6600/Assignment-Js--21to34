655fb6ec77eadf07ec288376adfcd876dc205b48
ayansheikh6600 <ayansheikh6600@email.com>
2023-03-31 01:47:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// console.log("Ayan")
+
+// Chapter 21 to 25
+// String Method
+
+// Question#1
+// var fisrtName = prompt("enter first name")
+// var lastName = prompt("Enter Last Name")
+// var fullName = fisrtName + lastName
+// console.log(fullName)
+
+// Question#2
+// var user = prompt("Enter Your Fav Phone Name")
+//  console.log("My Favorite phone is:" + user)
+//  console.log("length of string is:" + user.length)
+
+//Question#3
+// var nationality = "Pakistani"
+// console.log("String" + nationality)
+// console.log("index of n :" + nationality.indexOf("n"))
+
+//Question#4
+// var greetuser = "Hello World"
+// console.log("String" + greetuser)
+// console.log("last index of 'l' :" +greetuser.lastIndexOf("l"))
+
+//Question#5
+// var nationality = "Pakistani"
+// console.log("String" + nationality)
+// console.log("Character at index 3:" + nationality[3])
